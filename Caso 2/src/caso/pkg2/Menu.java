517191728228057eda40962780aac6423ba85784
Kyomitaka <kiyomi.takahashi.cr@gmail.com>
2024-08-14 20:23:17 -0600

    Add files via upload

---
+++ Caso 2/src/caso/pkg2/Menu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caso.pkg2;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Menu {
+    public Menu(int cantidad_habitaciones) {
+        for (int i = 1; i<= cantidad_habitaciones; i++) {
+            
+        }
+    }
+}
